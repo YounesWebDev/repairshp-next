c1c42c10fa3d49a4d2cd68604ef8df3f46e5650c
Younes <webdevyns@gmail.com>
2026-03-25 14:19:07 +0100

    first commit

---
+++ app/(rs)/layout.tsx
@@ -0,0 +1,15 @@
+
+export default async function RSLayout({
+    children,
+}:{
+    children: React.ReactNode
+}) {
+    return (
+        <div className="mx-auto w-full max-w-7xl">
+            {/* header */}
+            <div className="px-4 py-2">
+                {children}
+            </div>
+        </div>
+    )
+}
---
+++ app/(rs)/template.tsx
@@ -0,0 +1,15 @@
+
+export default async function Template({
+    children,
+}:{
+    children: React.ReactNode
+}) {
+    return (
+        <div className="animate-appear">
+            {/* header */}
+            <div className="px-4 py-2">
+                {children}
+            </div>
+        </div>
+    )
+}
